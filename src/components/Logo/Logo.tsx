2e3b99c8082d8bfe5109b77794a58bd68ee66115
mishamyrt <misha@myrt.co>
2023-09-21 22:13:47 +0300

    feat(Logo): add animation

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,7 +1,8 @@
 import clsx from 'clsx';
-import { type FC } from 'react';
+import { type FC, useState } from 'react';
 
 import logo from '$assets/logo.svg';
+import { useTimeout } from '$utils/hooks';
 
 import styles from './Logo.module.scss';
 
@@ -11,8 +12,17 @@ export interface LogoProps {
 }
 
 export const Logo: FC<LogoProps> = ({ dimmed, className }) => {
+  const [visible, setVisible] = useState(false);
+
+  useTimeout(() => {
+    setVisible(true);
+  }, 500);
+
   return (
-    <div className={clsx(styles.logo, { dimmed }, className)}>
+    <div className={clsx(styles.logo, {
+      dimmed,
+      [styles.visible]: visible,
+    }, className)}>
       <span className={styles.text}>Audición</span>
       <img src={logo} alt="Логотип Audición" />
     </div>
