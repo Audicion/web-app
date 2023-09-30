e1590a3ad85701c5e9f454d3d3ca56b186d46460
mishamyrt <misha@myrt.co>
2023-09-30 11:00:03 +0300

    refactor: simplify store usage

--- src/stores/navigation/actions.ts
+++ src/stores/navigation/actions.ts
@@ -8,3 +8,12 @@ export function setView(value: AppView) {
 export function setStep(value: number) {
   $step.set(value);
 }
+
+export function nextStep() {
+  setStep($step.get() + 1);
+}
+
+export function previousStep() {
+  const target = Math.max(0, $step.get() - 1);
+  setStep(target);
+}
