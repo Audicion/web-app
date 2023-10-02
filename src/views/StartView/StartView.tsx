ef287e619cf0e4b8e02852a6332779a3d197e40d
Ekaterina Makridi <makridi.ekaterina@gmail.com>
2023-10-02 23:46:38 +0300

    feat: add Stepper component

--- src/views/StartView/StartView.tsx
+++ src/views/StartView/StartView.tsx
@@ -1,17 +1,21 @@
+import { useStore } from '@nanostores/react';
 import { type FC } from 'react';
 
-import { Button } from '$components';
-import { nextStep, previousStep, setView } from '$stores/navigation';
+import { Button, Stepper } from '$components';
+import { $step, nextStep, previousStep, setView } from '$stores/navigation';
 import { useTitle } from '$utils/title';
 
 export const StartView: FC = () => {
   useTitle('Приготовьтесь');
+
+  const activeStep = useStore($step);
 
   return (
     <div className="view">
       <div className="view-content">
         <h1>Привет 🦊</h1>
       </div>
+      <Stepper active={activeStep}></Stepper>
       <div className="view-actions">
         <Button onClick={previousStep}>Минус</Button>
         <Button onClick={nextStep}>Плюс</Button>
