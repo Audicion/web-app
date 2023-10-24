33369fcccbf9ab7f9cb0adcffbc1cbd960e635a2
Ekaterina Makridi <makridi.ekaterina@gmail.com>
2023-10-24 22:26:16 +0300

    feat(StartView): add content

--- src/views/StartView/StartView.tsx
+++ src/views/StartView/StartView.tsx
@@ -1,14 +1,8 @@
 import { useStore } from '@nanostores/react';
 import { type FC } from 'react';
 
-import { Button, Card } from '$components';
-import {
-  $step,
-  nextStep,
-  previousStep,
-  setView,
-  STEP_COUNT,
-} from '$stores/navigation';
+import { Button, Card, Typography } from '$components';
+import { $step, setView, STEP_COUNT } from '$stores/navigation';
 import { useTitle } from '$utils/title';
 
 export const StartView: FC = () => {
@@ -18,16 +12,21 @@ export const StartView: FC = () => {
 
   return (
     <Card
-      title="Привет 🦊"
-      actions={
-        <>
-          <Button onClick={previousStep}>Минус</Button>
-          <Button onClick={nextStep}>Плюс</Button>
-          <Button onClick={() => setView('measure')}>Далее</Button>
-        </>
-      }
+      title="Приготовьтесь к проверке 🚀"
+      actions={<Button onClick={() => setView('measure')}>Начать</Button>}
       stepCount={STEP_COUNT}
       activeStep={activeStep}
-    ></Card>
+    >
+      <Typography>
+        Чтобы получить точный эффект используйте максимальный уровень громкости на
+        наушниках и производите замер на одном и том же оборудовании (воспроизводящее
+        устройство и наушники).
+      </Typography>
+      <Typography>
+        Разные наушники и воспроизводящие устройства имеют разную АЧХ, поэтому нельзя
+        сравнивайте результаты полученные с разных устройств.
+      </Typography>
+      <Typography color="secondary">Проверка займёт около 5 минут.</Typography>
+    </Card>
   );
 };
